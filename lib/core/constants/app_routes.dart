76a7e09dbb8a6be0c69ab3ee1fed1258add809c7
Nada Mohamed <nadamoafia668@gmail.com>
2026-09-17 22:37:18 +0300

    refactor: replace per-screen routing with single PageView for onboarding

--- lib/core/constants/app_routes.dart
+++ lib/core/constants/app_routes.dart
@@ -1,8 +1,5 @@
 class AppRoutes {
   AppRoutes._();
-
-  static const String introScreen1 = '/intro-screen-1';
-  static const String introScreen2 = '/intro-screen-2';
-  static const String introScreen3 = '/intro-screen-3';
-  static const String introScreen4 = '/intro-screen-4';
+  static const String mainOnboarding = '/onboarding-screen';
+  static const String loginScreen = '/login-screen';
 }
---
+++ lib/features/ui/onboarding/pages/onboarding_screen.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+import 'package:safe_rag/core/constants/app_routes.dart';
+
+import 'onboarding_screen_1.dart';
+import 'onboarding_screen_2.dart';
+import 'onboarding_screen_3.dart';
+import 'onboarding_screen_4.dart';
+
+class OnboardingScreen extends StatefulWidget {
+  const OnboardingScreen({super.key});
+
+  @override
+  State<OnboardingScreen> createState() => _OnboardingScreenState();
+}
+
+class _OnboardingScreenState extends State<OnboardingScreen> {
+  final _pageController = PageController();
+
+  void _next() {
+    _pageController.nextPage(
+      duration: const Duration(milliseconds: 300),
+      curve: Curves.easeInOut,
+    );
+  }
+
+  void _skipToLogin() {
+    Navigator.pushReplacementNamed(context, AppRoutes.loginScreen);
+  }
+
+  @override
+  void dispose() {
+    _pageController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return PageView(
+      controller: _pageController,
+      physics: NeverScrollableScrollPhysics(),
+      children: [
+        OnboardingScreen1(onContinue: _next),
+        OnboardingScreen2(onContinue: _next, onSkip: _skipToLogin),
+        OnboardingScreen3(onContinue: _next, onSkip: _skipToLogin),
+        OnboardingScreen4(onContinue: _skipToLogin),
+      ],
+    );
+  }
+}
